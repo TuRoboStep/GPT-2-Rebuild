90105300a5ae83d9af43b0044e3b8b1c01ed8192
Stephan Meyer <stm@autonomos.xyz>
2024-06-20 09:19:20 +0200

    enable weight loading

--- train_gt2.py
+++ train_gt2.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
 import torch
+import math
 import torch.nn as nn
 from torch.nn import functional as F
 
 @dataclass
-class GTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embeding dimension
 
 
 class GPT(nn.Module):
@@ -26,6 +27,57 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', "gpt2-medium", "gpt2-large", "gpt-xl"}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            "gpt2":         dict(n_layer=12, n_head=12, n_embd=768), # 124M params
+            "gpt2-medium":  dict(n_layer=24, n_head=16, n_embd=1024), # 3504M params
+            "gpt2-large":   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            "gpt2-xl":      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GP model checkpoints
+        config_args['block_size'] = 1024 # always 50257 for GP model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer
+
+        # init a hugginfface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]
+        transposed = ['attn.c_attn.weight', 'attn.cproj.weight', 'mlp.c_fc.weight', "mlp.c_proj.weight"]
+        # basically the openai checkpoints use a "Conv1" module, but we only want to use a vanilla versio
+        # this means, that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+
 class Block(nn.Module):
 
     def __init__(self, config):
@@ -41,3 +93,64 @@ class Block(nn.Module):
         return x
 
 
+class CausalSelfAttention(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd)
+        # output_projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularizatio
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # not really a 'bias', more of a mask, but following die OpenAI/HF naming though
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+    
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values, for all heads in batch and move head forward to be the batch
+        # nh is "number of heads", hs is "head size", and C (number of chanels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs = 6, so nh*hs = 768 channels in the Transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        # transform tensor so pytorch treats (B, nh) as "batch dimension" and applies all operations
+        # to those dimensions in parallel (all the batches and all the heads)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # attention (materializes the larte (T,T) matric for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # mask out future attentions
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        # normalize the attention so it sums to 1
+        att = F.softmax(att, dim=-1)
+        # weighted sum of all the values that the net finds interesting
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        # re-assemble all head outputs side by side, in essence concatenation
+        y = y.transpose(1, 2).contiguous().view(B, T, C) 
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc   = nn.Linear(config.n_embd, 4*config.n_embd)
+        self.gelu   = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4*config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+# ------------------------------------------------------------------------
+model = GPT.from_pretrained('gpt2')
+print("didn't crash yay!")
