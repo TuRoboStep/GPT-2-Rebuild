f46ad141d74513432e0531717a18a0a271ad17f7
Stephan Meyer <stm@autonomos.xyz>
2024-07-03 22:23:07 +0200

    fix dataset_loader test and execute unit tests with poetry env in CICD

--- tests/test_data_loader_lite.py
+++ tests/test_data_loader_lite.py
@@ -4,17 +4,23 @@ import torch
 import pytest
 
 dataset_config = DatasetConfig(dataset_path='tests/data/test_dataset.txt', encoding='gpt2')
-data_loader = DataLoaderLite(B=2, T=4, dataset_config=dataset_config)
 
-def test_data_loader_lite():
-    assert data_loader.B == 2
-    assert data_loader.T == 16
+@pytest.mark.parametrize("data_loader, expected_B, expected_T, expected_tokens",
+                         [
+                             pytest.param(DataLoaderLite(B=2, T=16, dataset_config=dataset_config),
+                                          2,
+                                          16,
+                                          torch.tensor([9288,27039,198,198,2220,340,11,15458,340,11,
+                                               1332,477,6982,286,1243,13,198,198,14150,1257,
+                                               14373,198]))
+                         ])
+def test_data_loader_lite(data_loader: DataLoaderLite, expected_B: int, expected_T: int, expected_tokens: torch.Tensor):
+    assert data_loader.B == expected_B
+    assert data_loader.T == expected_T
 
     data_loader._load_data()
     assert data_loader.tokens is not None
-    assert torch.all(data_loader.tokens.eq(torch.tensor([9288,27039,198,198,2220,340,11,15458,340,11,
-                                               1332,477,6982,286,1243,13,198,198,14150,1257,
-                                               14373,198])))
+    assert torch.all(data_loader.tokens.eq(expected_tokens))
 
 @pytest.mark.parametrize("data_loader, expected_x, expected_y, expected_next_curser_position",
                          [
